240d4e9d025f093407dedb9934a1237a76abbc69
alvienzo720 <hudson1Dellmac>
2023-05-09 18:51:52 +0300

    Modify HathatConf

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: `https://sepolia.infura.io/v3/YOUR-INFURA-PROJECT-ID`,
       chainId: 4, // Sepolia uses chainId 4
-      accounts: ['db50acc44a9eee0a59abf844f18703eb1c5784f8a2606f4d73ba622fab7024b6'], // Your private key
+      accounts: [''], // Your private key
       gasPrice: 20000000000, // Optional: you can set a custom gas price
     },
   },
